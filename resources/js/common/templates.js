21557677cc37cf6e4d98135e06152c79d69ad9b1
yangxiaowei <yangxiaowei@cleartv.cn>
2016-04-12 18:35:06 +0800

    完成频道列表页面部分

--- resources/js/common/templates.js
+++ resources/js/common/templates.js
@@ -1,7 +1,17 @@
-define([], function(require, exports, module){exports['a']=function(obj){
+define(function(require, exports, module){exports['channel_table_row']=function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='mmmss';
+__p+='<tr data-channel-id="'+
+((__t=(id))==null?'':__t)+
+'"> <td><input type="checkbox"></td> <td>频道名称:'+
+((__t=(name))==null?'':__t)+
+'</td> <td>调度类型:'+
+((__t=(schedule_type))==null?'':__t)+
+'</td> <td>调度参数:'+
+((__t=(schedule_params))==null?'':__t)+
+'</td> <td>版本:'+
+((__t=(version))==null?'':__t)+
+'</td> <td><button class="btn-channel-detail"></button></td> </tr>';
 }
 return __p;
 };});
--- resources/js/pages/channel/list.js
+++ resources/js/pages/channel/list.js
@@ -1,10 +1,102 @@
 'use strict';
 
 define(function(require, exports, module) {
-	
-	var templates = require('common/templates'),
-		config = require('common/config');
-	
-	exports.init = function() {};
+
+    // depend on these components
+    var templates = require('common/templates'),
+        config    = require('common/config'),
+        util      = require('common/util');
+
+    // global variables
+    var requestUrl    = 'http://192.168.18.166',
+        projectName   = 'develop',
+        nDisplayItems = 25;
+
+    // 初始化页面
+	exports.init = function() {
+        loadPage(1);
+        registerEventListeners();
+    };
+
+    function registerEventListeners() {
+        $('#channel-table').delegate('input[type="checkbox"]', 'click', function (ev) {
+        });
+        $('#channel-table').delegate('tr', 'click', function (ev) {
+        });
+        $('#channel-table').delegate('.btn-channel-detail', 'click', function (ev) {
+        });
+        $('#channel-list-controls .select-all').click(function (ev) {
+        });
+        $('#channel-list-controls .btn-publish').click(function (ev) {
+        });
+        $('#channel-list-controls .btn-publish-later').click(function (ev) {
+        });
+        $('#channel-list-controls .btn-copy').click(function (ev) {
+        });
+        $('#channel-list-controls .btn-delete').click(function (ev) {
+        });
+
+    }
+
+    // 加载页面数据
+    function loadPage(pageNum) {
+        var pager = {
+            page: String(pageNum),
+            total: '0',
+            per_page: String(nDisplayItems),
+            orderby: 'ID',
+            sortby: '',
+            keyword: ''
+        };
+        var data = JSON.stringify({
+            Action: 'GetPage',
+            Project: projectName,
+            Pager: pager
+        });
+        util.ajax('post', requestUrl + '/backend_mgt/v1/channels', data, function (res) {
+            render(res);
+        });
+    }
+
+    // 渲染界面
+    function render(json) {
+
+        var totalPages = Math.ceil(json.Pager.total / nDisplayItems);
+        $('#channel-table-pager').jqPaginator({
+            totalPages: totalPages,
+            visiblePages: 10,
+            currentPage: Number(json.Pager.page),
+            onPageChange: function (num, type) {
+                if (type === 'change') {
+                    loadPage(num);
+                }
+            }
+        });
+
+        $('#channel-table>tbody').html('');
+        json.Channels.forEach(function (el, idx, arr) {
+            var schedule_type = el.Overall_Schedule_Type === 'Regular' ? '常规' : '定时';
+            var schedule_params = {
+                'Sequence': '顺序',
+                'Percent': '比例',
+                'Random': '随机'
+            }[el.Overall_Schedule_Paras.Type];
+            schedule_params = schedule_params ? schedule_params : '其它';
+            var data = {
+                id: el.ID,
+                name: el.Name,
+                schedule_type: schedule_type,
+                schedule_params: schedule_params,
+                version: el.Version
+            };
+            $('#channel-table>tbody').append(templates.channel_table_row(data));
+        });
+
+        $('#channel-table input[type="checkbox"]').iCheck({
+            checkboxClass: 'icheckbox_flat-blue',
+            radioClass: 'iradio_flat-blue'
+        });
+
+    }
 	
 });
