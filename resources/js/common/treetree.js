f3e4204916a456bbb348d92f437ab4f989e54fa9
emmaga <emma_0812@msn.cn>
2016-04-15 18:09:07 +0800

    树的操作部分更新

--- resources/js/common/treetree.js
+++ resources/js/common/treetree.js
@@ -17,49 +17,70 @@ define(function(require, exports, module) {
 	        return;
 	      }
 
-	      createLeaf(dom, data);
+	      // 创建树
+	      tree.createNode(dom, data);
+
+	      //focus第一个结点
 	      var li = dom.find('li:nth(0)');
 	      li.addClass('open');
 	      li.addClass('focus');
 	      li.children('a').find('i.fa-folder-o').removeClass('fa-folder-o').addClass('fa-folder-open-o');
 
-	      function createLeaf(dom, data){
+	      // 树的展开收起
+	      $('#'+ tree.domId +' li .fa-angle-right').each(function(i, e){
+	        $(this).click(function(e){
+	        	e.preventDefault();
+  		      e.stopPropagation();
+	        	var li = $(this).parent().parent();
+						if (li.hasClass('open')) {
+							li.removeClass('open');
+						}else{
+							li.addClass('open');
+						}
+	        })
+	      })
+	    }
 
-	        // 是否可选中
-	        var checkbox = '';
-	        if(tree.canCheck === true){
-	          checkbox = '<input type="checkbox">';
-	        }
+	    tree.setFocus = function(dom){
+	    	$('#'+tree.domId).find('.focus').removeClass('focus');
+        dom.addClass('focus');
+	    }
 
-	        for(var i = 0; i < data.length; i++){
-	          var treeview = '';
-	          var angle = '';
+	    tree.createNode = function(dom, data){
 
-	          // 是否有孩子
-	          if(data[i].children.length > 0){
-	            treeview = 'treeview';
-	            angle = '<i class="fa fa-angle-right"></i>';
-	          }
+        // 是否可选中
+        var checkbox = '';
+        if(tree.canCheck === true){
+          checkbox = '<input type="checkbox">';
+        }
 
-	          var li = $('' +
-	            '<li id="' + data[i].id + '" class="' + treeview + '">' +
-	              '<a>' + checkbox + angle +
-	                '<i class="fa fa-folder-o"></i> ' + data[i].name +
-	              '</a>' +
-	            '</li>');
+        for(var i = 0; i < data.length; i++){
+          var treeview = '';
+          var angle = '';
 
-	          dom.append(li);
+          // 是否有子结点
+          if(data[i].children.length > 0){
+            treeview = 'treeview';
+            angle = '<i class="fa fa-angle-right"></i>';
+          }
 
-	          if(treeview === 'treeview'){
-	            var ul = $('<ul class="tree-menu-2"></ul>');
-	            li.append(ul);
-	            createLeaf(ul, data[i].children);
-	          }
-	        }
+          var li = $('' +
+            '<li node-id="' + data[i].id + '" class="' + treeview + '">' +
+              '<a>' + checkbox + angle +
+              '<i class="fa fa-folder-o"></i><span> ' + data[i].name + '</span>' +
+              '</a>' +
+            '</li>');
 
-	      }
+          dom.append(li);
 
-	    };
+          if(treeview === 'treeview'){
+            var ul = $('<ul class="tree-menu-2"></ul>');
+            li.append(ul);
+            tree.createNode(ul, data[i].children);
+          }
+        }
+
+      }
 
 	    return tree;
 	  }
--- resources/js/pages/terminal/list.js
+++ resources/js/pages/terminal/list.js
@@ -1,29 +1,157 @@
 define(function(require, exports, module) {
 
   var TREE = require("common/treetree.js");
-  var UTIL = require("common/util.js");
   var CONFIG = require("common/config.js");
+  var UTIL = require("common/util.js");
 
 	exports.init = function(){
-		
+    initTree();
+	}
+
+  function initTree(){
+
+    var dataParameter = {
+      "project_name": CONFIG.projectName,
+      "action": "getTree"
+    };
+
     UTIL.ajax(
       'POST', 
       CONFIG.serverRoot+'/backend_mgt/v2/termcategory', 
-      {
-        "project_name": UTIL.getCookie('project_name'),
-        "action": "getTree"
-      },
+      JSON.stringify(dataParameter),
       function(data){
         if(data.rescode === '200'){
           data = data.TermTree.children;
           var tree = {domId: 'termclass-tree', canCheck: false};
           tree = TREE.new(tree);
           tree.createTree($('#'+tree.domId), data);
+
+          // 终端分类列表各项点击
+          $('#termclass-tree li > a').each(function(i, e){
+            $(this).click(function(e){
+              tree.setFocus($(this).parent());
+               alert($(this).parent().attr('node-id'))
+            })
+          })
+
+          // 添加终端分类按钮点击
+          $('#tct_add').click(function(){
+            var li = $('#termclass-tree').find('.focus');
+            var newNode = [
+              {
+                "children": [], 
+                "id": "", 
+                "name": "未命名终端分类"
+              }
+            ]
+            // 如果分类有子分类
+            var ul;
+            if(li.hasClass('treeview')){
+              ul = li.children('ul');
+            }
+            // 如果分类下无子分类
+            else{
+              ul = $('<ul class="tree-menu-2"></ul>');
+              li.append(ul);
+            }
+            tree.createNode(ul, newNode);
+            
+          })
+
+          // 编辑终端分类按钮点击
+          $('#tct_edit').click(function(){
+            var p = $('#termclass-tree').find('.focus');
+            var t = p.children('a').find('span');
+            if(t.css('display') === 'none'){
+              var input = p.children('a').find('div > input');
+              input.focus();
+              return;
+            }
+            t.css('display', 'none');
+
+            var input_div = $('' + 
+            '<div class="input-group tree-input-group">' +
+              '<input type="text" class="form-control">' +
+              '<span class="input-group-addon"><i class="fa fa-check"></i></span>' +
+            '</div>');
+
+            var input = input_div.find('input');
+            var done = input_div.find('span');
+            input.val($.trim(t.html()));
+            p.children('a').append(input_div);
+            input.focus();
+
+            input.click(function(e){
+              e.preventDefault();
+              e.stopPropagation();
+            })
+
+            input.blur(function(e){
+              editTermClassName();
+            })
+
+            done.click(function(e){
+              e.preventDefault();
+              e.stopPropagation();
+            })
+
+            function editTermClassName(){
+              var change = $.trim(input.val());
+              var a = input.parent().parent();
+              var t = a.children('span');
+              
+              // 终端组分类名称为空时恢复原名称
+              if(change === ''){
+                alert('终端分类名称不能为空');
+                t.css('display','inline-block');
+                input.parent().remove();
+              }
+
+              // 终端组分类名称未改变时不提交修改
+              if( change ===  $.trim(t.html()) ){
+                t.css('display','inline-block');
+                input.parent().remove();
+              }
+
+              // 提交终端组分类名称修改
+              else{
+                var nodeId = a.parent().attr('node-id');
+                var data = {
+                  "project_name": CONFIG.projectName,
+                  "action": "changeCategoryName",
+                  "categoryID": nodeId,
+                  "newName": change
+                }
+
+                UTIL.ajax(
+                  'POST', 
+                  CONFIG.serverRoot + '/backend_mgt/v2/termcategory',
+                  JSON.stringify(data), 
+                  function(data){
+                    var a = $('#termclass-tree').find('.focus').children('a');
+                    var input = a.children('div').children('input');
+                    var t = a.children('span');
+                    if(data.rescode == '200'){
+                      t.html(' ' + $.trim(input.val()));
+                      t.css('display','inline-block');
+                      input.parent().remove();
+                    }else{
+                      alert('编辑终端分类失败');
+                      input.focus();
+                    }
+                  }
+                );
+              }
+            }
+
+          })
+
         }else{
           alert('获取终端分类失败');
         }
       }
     );
+
     /*var tree = {domId: 'termclass-tree', canCheck: false};
     tree = TREE.new(tree);
     var data = [
@@ -129,27 +257,18 @@ define(function(require, exports, module) {
     tree.createTree($('#'+tree.domId), data);
     */
 
+    // $('.treetree li .fa-angle-left').each(function(i, e){
+    //   $(this).click(function(e){
+    //     alert(1)
+    //   })
+    // })
 
-	}
-
-  // $('#test').click(function(e){
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  // })
-
-  // function initTreetree(){
-  //   $('.treetree li .fa-angle-left').each(function(i, e){
-  //     $(this).click(function(e){
-  //       alert(1)
-  //     })
-  //   })
-
-  //   $('.treetree li').each(function(i, e){
-  //     $(this).click(function(e){
-  //       e.preventDefault();
-  //       e.stopPropagation();
-  //     })
-  //   })
-  // }
+    // $('.treetree li').each(function(i, e){
+    //   $(this).click(function(e){
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    //   })
+    // })
+  }
 
 });
