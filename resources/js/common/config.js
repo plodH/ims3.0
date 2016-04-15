cfb0f6bcb9c09a40b9b9c269ae0b6dbf4ff401a0
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-04-15 18:36:39 +0800

    2016-04-15 陈晓斌 配置修改

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
 
 	exports.serverRoot = 'http://192.168.18.166';
-	// exports.serverRoot = '../../testdata/';
+	exports.Resource_UploadURL = "http://imsresource.cleartv.cn/upload";
 
 });
