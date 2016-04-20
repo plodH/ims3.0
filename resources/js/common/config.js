c550f9f6f304b5f572367323a95a6bc757607868
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-04-20 10:36:02 +0800

    2016-04-20 陈晓斌 格式

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,20 +1,20 @@
-define(function(require, exports, module) {
-	var UTIL = require("common/util.js");
-	
-	exports.serverRoot = 'http://192.168.18.166';
-	exports.Resource_UploadURL = "http://imsresource.cleartv.cn/upload";
-  exports.projectName = UTIL.getCookie('project_name');
-  exports.termListLoadInterval = 60 * 1000;
-  exports.pager = {
-    pageSize: 15,
-    visiblePages: 10,
-    first:'<li><a href="javascript:;">首页</a></li>',
-    prev:'<li><a href="javascript:;">上一页</a></li>',
-    next:'<li><a href="javascript:;">下一页</a></li>',
-    last:'<li><a href="javascript:;">末页</a></li>',
-    page:'<li><a href="javascript:;">{{page}}</a></li>'
-  }
+define(function (require, exports, module) {
+    var UTIL = require("common/util.js");
 
-	// exports.serverRoot = '../../testdata/';
+    exports.serverRoot = 'http://192.168.18.166';
+    exports.Resource_UploadURL = "http://imsresource.cleartv.cn/upload";
+    exports.projectName = UTIL.getCookie('project_name');
+    exports.termListLoadInterval = 60 * 1000;
+    exports.pager = {
+        pageSize: 15,
+        visiblePages: 10,
+        first: '<li><a href="javascript:;">首页</a></li>',
+        prev: '<li><a href="javascript:;">上一页</a></li>',
+        next: '<li><a href="javascript:;">下一页</a></li>',
+        last: '<li><a href="javascript:;">末页</a></li>',
+        page: '<li><a href="javascript:;">{{page}}</a></li>'
+    }
+
+    // exports.serverRoot = '../../testdata/';
 
 });
