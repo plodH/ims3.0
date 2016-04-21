281c94dbe3e9c511145889f9f41c6f9fcb0fd48d
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-04-21 16:54:53 +0800

    2016-04-21 陈晓斌 ckeditor 配置

--- resources/plugins/ckeditor/config.js
+++ resources/plugins/ckeditor/config.js
@@ -5,6 +5,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 
 CKEDITOR.editorConfig = function( config ) {
+config.font_names='宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;'+ config.font_names;
 config.toolbar =
 [
     ['Source'],
